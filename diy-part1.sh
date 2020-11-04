a86d4354b0eaee4eb6ffdeffecfb18862a028f17
Tokimeki <35131120+TokimekiMemorial@users.noreply.github.com>
2020-11-04 11:08:56 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
